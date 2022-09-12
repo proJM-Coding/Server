30f34240c54402c961a78430d5afcbca92a798e6
proJM <81658610+proJM-Coding@users.noreply.github.com>
2022-09-12 12:35:11 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 var id = Math.floor(Math.random() * 10) + "" + Math.floor(Math.random() * 10) + "" + Math.floor(Math.random() * 10) + "" + Math.floor(Math.random() * 10) + "" + Math.floor(Math.random() * 10);
 
+function Email(){
+  var nodemailer = require('nodemailer');
 
-var nodemailer = require('nodemailer');
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: SEND_EMAIL,
+      pass: EMAIL_PASS
+    }
+  });
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: SEND_EMAIL,
-    pass: EMAIL_PASS
-  }
-});
+  var mailOptions = {
+    from: SEND_EMAIL,
+    to: RECEIVE_EMAIL,
+    subject: 'Session Id:',
+    text: id
+  };
 
-var mailOptions = {
-  from: SEND_EMAIL,
-  to: RECEIVE_EMAIL,
-  subject: 'Session Id:',
-  text: id
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+   } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 };
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
