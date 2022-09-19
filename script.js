0353f2e29bbf5d36785d948d5802c3aadb8043bd
proJM <81658610+proJM-Coding@users.noreply.github.com>
2022-09-19 17:19:34 +0930

    Update script.js

--- script.js
+++ script.js
@@ -30,16 +30,8 @@ function Email(){
   });
 };
 
-async function startCapture(displayMediaOptions) {
-  let captureStream = null;
-
-  try {
-    captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-  } catch (err) {
-    console.error(`Error: ${err}`);
-  }
-  return captureStream;
-  return navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+function startCapture(displayMediaOptions) {
+ return navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
     .catch((err) => { console.error(`Error:${err}`); return null; });
 }
 
